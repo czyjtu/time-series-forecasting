f6a1b60752fe9e1cace11905f7a1a0cd453c1cdc
gregori0o <glegeza@wp.pl>
2023-04-15 23:05:02 +0200

    add prophet predictor

--- utils/predictors.py
+++ utils/predictors.py
@@ -110,8 +110,8 @@ class ESNPredictor(BasePredictor):
 class SARIMAPredictor(BasePredictor):
     def __init__(
         self,
-        order: tuple,
-        seasonal_order: tuple = (0, 0, 0, 0), # by default without seasonality
+        order: tuple, # (p, d, q)
+        seasonal_order: tuple = (0, 0, 0, 0), # (P, D, Q, s), by default without seasonality
     ):
         self.model_params = {"order": order, "seasonal_order": seasonal_order}
 
@@ -133,8 +133,60 @@ class SARIMAPredictor(BasePredictor):
         if self.start_forecast is None:
             raise ValueError("Must call fit before forecast")
         
-        predictions = self._arima.predict(start=self.start_forecast, end=self.start_forecast+horizon)
+        predictions = self._arima.predict(start=self.start_forecast, end=self.start_forecast + horizon - 1)
         return predictions
+
+    def load(self, path: str):
+        raise NotImplementedError
+    
+
+class ProphetPredictor(BasePredictor):
+    def __init__(
+        self,
+        changepoint_prior_scale: float = 0.05,
+        seasonality_prior_scale: float = 10,
+        seasonality_mode: str = "additive",
+        daily_seasonality: bool = True,
+        weekly_seasonality: bool = True,
+        yearly_seasonality: bool = True,
+        custom_seasonality: int | None = None, # in days
+    ):
+        self._prophet = Prophet(
+            changepoint_prior_scale=changepoint_prior_scale, 
+            seasonality_prior_scale=seasonality_prior_scale, 
+            seasonality_mode=seasonality_mode,
+            daily_seasonality=daily_seasonality,
+            weekly_seasonality=weekly_seasonality,
+            yearly_seasonality=yearly_seasonality,
+            )
+        if custom_seasonality:
+            self._prophet.add_seasonality(
+                name=f"{custom_seasonality} days",
+                period=custom_seasonality,
+                fourier_order=10)
+
+        self.is_fitted: bool = False
+
+    @property
+    def model(self) -> DeepESN:
+        if self._prophet is None:
+            raise ValueError("Must call fit before model")
+        return self._prophet
+
+    def fit(self, X: pd.DataFrame): # DataFrame must have `y` and `ds` columns
+        self._prophet.fit(X)
+        self.is_fitted = True
+
+    def forecast(self, horizon: int | pd.DataFrame) -> np.ndarray: # horizon can be a DataFrame with `ds` column
+        if not self.is_fitted:
+            raise ValueError("Must call fit before forecast")
+        
+        if isinstance(horizon, int):
+            future = self._prophet.make_future_dataframe(periods=horizon, include_history=False)
+        else:
+            future = horizon
+        predictions = self._prophet.predict(future)
+        return predictions.yhat.values
 
     def load(self, path: str):
         raise NotImplementedError
