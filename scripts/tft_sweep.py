df1f93eb81fc842ef365df2ad14741f93938c64b
czyjtu <maciejczyjtu@gmail.com>
2023-06-16 21:03:43 +0200

    added training script for best sweep model

--- scripts/tft_sweep.py
+++ scripts/tft_sweep.py
@@ -3,7 +3,7 @@ from darts import TimeSeries
 from darts.dataprocessing.transformers import Scaler, MissingValuesFiller
 from darts.models import TFTModel
 from darts.utils.likelihood_models import QuantileRegression
-from darts.metrics.metrics import mae
+from darts.metrics.metrics import rmse
 import numpy as np
 import torch as th
 from darts import TimeSeries, concatenate
@@ -21,9 +21,10 @@ from pytorch_lightning.callbacks import LearningRateMonitor, TQDMProgressBar
 import matplotlib.pyplot as plt
 from utils import data_loading as dl
 from utils import predictors as pred
-
+import seaborn as sns
 
 SCRATCH_DIR = Path(os.environ.get("SCRATCH_LOCAL", "."))
+MODEL_DIR = Path("./models/sweep2")
 
 
 @dataclass
@@ -33,19 +34,13 @@ class HParams:
     hidden_size: int
     num_heads: int
     full_attention: bool
+    input_chunk_length: int
+    output_chunk_length: int
 
 
-Trainer()
-
-
-def score_tft(
+def get_model(
     hparams: HParams,
-    train: TimeSeries,
-    val: TimeSeries,
-    fut_covariates: TimeSeries,
     epochs: int,
-    input_chunk_length: int,
-    output_chunk_length: int,
     dataset_name: str,
 ):
     quantiles = [0.1, 0.25, 0.5, 0.75, 0.9]
@@ -59,8 +54,8 @@ def score_tft(
 
     model = TFTModel(
         force_reset=True,
-        input_chunk_length=input_chunk_length,
-        output_chunk_length=output_chunk_length,
+        input_chunk_length=hparams.input_chunk_length,
+        output_chunk_length=hparams.output_chunk_length,
         hidden_size=hparams.hidden_size,
         lstm_layers=1,
         num_attention_heads=hparams.num_heads,
@@ -79,6 +74,23 @@ def score_tft(
         work_dir=SCRATCH_DIR / "tft_sweep",
         model_name=f"tft_{dataset_name}_{hparams.lr}_{hparams.batch_size}_{hparams.hidden_size}_{hparams.num_heads}_{hparams.full_attention}",
     )
+    return model
+
+
+def score_tft(
+    hparams: HParams,
+    train: TimeSeries,
+    val: TimeSeries,
+    fut_covariates: TimeSeries,
+    epochs: int,
+    dataset_name: str,
+):
+    model = get_model(
+        hparams,
+        epochs,
+        dataset_name,
+    )
+
     model.fit(
         train,
         future_covariates=fut_covariates,
@@ -89,45 +101,24 @@ def score_tft(
         num_loader_workers=4,
     )
     horizon = len(val)
-    pred_series = model.predict(n=horizon, num_samples=30)
+    pred_series = model.predict(n=horizon, num_samples=100)
 
-    mae_score = mae(pred_series, val)
-
-    # y_hat_test = pred_series.mean().values()
-    # y_test = val.values()
-    # y_train = train.values()[-input_chunk_length:]
-    # xrange_real = np.arange(y_train.shape[0] + y_test.shape[0])
-    # xrange_pred = np.arange(y_train.shape[0], y_train.shape[0] + y_test.shape[0])
-    # wandb.log(
-    #     {
-    #         "val_pred": wandb.plot.line_series(
-    #             xs=[xrange_real, xrange_pred],
-    #             ys=[np.concatenate([y_train, y_test]), y_hat_test],
-    #             keys=["true_series", "predicted"],
-    #             title="predictions",
-    #             xname="ts",
-    #         )
-    #     }
+    rmse_score = rmse(pred_series, val)
+    # plot_predictions(
+    #     train.values(),
+    #     val.values(),
+    #     pred_series.mean().values(),
+    #     "predictions",
+    #     f"rmse={rmse_score}",
+    #     save=False,
     # )
+    # wandb.log({"val_plot": plt})
 
-    return mae_score
+    return rmse_score
 
 
-def entrypoint(dataset):
-    if dataset == "sunspots":
-        data = dl.DataLoader(dl.DATASET.SUNSPOTS, use_test=False)
-        input_len, output_len = 400, 133
-    elif dataset == "electricity":
-        data = dl.DataLoader(dl.DATASET.ELECTRICITY, use_test=False)
-        input_len, output_len = 168, 24
-    elif dataset == "mackey_glass":
-        data = dl.DataLoader(dl.DATASET.MACKEY_GLASS, use_test=False)
-        input_len, output_len = 100, 50
-    elif dataset == "temperature":
-        data = dl.DataLoader(dl.DATASET.TEMPERATURE, use_test=False)
-        input_len, output_len = 90, 30
-    else:
-        raise ValueError(f"Unknown dataset: {dataset}")
+def get_series_and_covariates(dataset, use_test=False):
+    data = dl.DataLoader(dataset, use_test=use_test)
 
     covariates_names = {
         dl.DATASET.ELECTRICITY: ["hour", "weekday", "month", "year", "day"],
@@ -137,13 +128,36 @@ def entrypoint(dataset):
     }[data.name]
     train, val, covariates = pred.get_series_from_dataset(data, covariates_names)
 
+    return train, val, covariates
+
+
+INPUTS_DIMENSIONS = {
+    dl.DATASET.SUNSPOTS: (400, 133),
+    dl.DATASET.ELECTRICITY: (168, 24),
+    dl.DATASET.MACKEY_GLASS: (100, 50),
+    dl.DATASET.TEMPERATURE: (90, 30),
+}
+
+
+def entrypoint(dataset):
     wandb.init(project="tft_sweep", entity="czyjtu")
+    train, val, covariates = get_series_and_covariates(dataset, False)
+    input_chunk_length, output_chunk_length = INPUTS_DIMENSIONS[dataset]
+    print(
+        "validation length",
+        len(val),
+        "input_chunk_length",
+        wandb.config.input_chunk_length,
+    )
+
     hparams = HParams(
         lr=wandb.config.lr,
         batch_size=wandb.config.batch_size,
         hidden_size=wandb.config.hidden_size,
         num_heads=wandb.config.num_heads,
         full_attention=wandb.config.full_attention,
+        input_chunk_length=wandb.config.input_chunk_length,
+        output_chunk_length=output_chunk_length,
     )
 
     score = score_tft(
@@ -151,33 +165,134 @@ def entrypoint(dataset):
         train,
         val,
         covariates,
-        epochs=10,
-        input_chunk_length=input_len,
-        output_chunk_length=output_len,
+        epochs=20,
         dataset_name=dataset,
     )
-    wandb.log({"mae_score": score, "hparams": asdict(hparams)})
+    wandb.log({"rmse_score": score, "hparams": asdict(hparams)})
+
+
+def plot_predictions(y_train, y_test, y_hat_test, plot_name, title=None, save=True):
+    xrange_train = np.arange(y_train.shape[0])
+    xrange_test = np.arange(y_train.shape[0], y_train.shape[0] + y_test.shape[0])
+    sns.lineplot(y=y_train, x=xrange_train, label="train")
+    sns.lineplot(y=y_test, x=xrange_test, label="test")
+    if y_hat_test is not None:
+        sns.lineplot(y=y_hat_test, x=xrange_test, label="prediction")
+    # plot vertical dotted line
+    plt.axvline(x=y_train.shape[0], linestyle="--", color="black")
+    if title:
+        plt.title(title)
+    plt.legend()
+    if save:
+        plt.savefig(f"{plot_name}.png")
 
 
 @click.command()
 @click.option("--dataset", help="dataset name")
-def main(dataset):
-    sweep_configuration = {
-        "name": f"tft_sweep_{dataset}",
-        "method": "bayes",
-        "metric": {"goal": "minimize", "name": "mae_score"},
-        "parameters": {
-            "lr": {"values": [0.1, 0.01, 0.001]},
-            "batch_size": {"values": [64, 128, 256]},
-            "hidden_size": {"values": [16, 32, 64]},
-            "num_heads": {"values": [2, 4, 6]},
-            "full_attention": {"values": [True, False]},
-        },
-    }
+@click.option("--sweep_id", required=False)
+def main(dataset, sweep_id):
+    dataset = {
+        "s": dl.DATASET.SUNSPOTS,
+        "t": dl.DATASET.TEMPERATURE,
+        "e": dl.DATASET.ELECTRICITY,
+        "m": dl.DATASET.MACKEY_GLASS,
+    }[dataset]
 
-    sweep_id = wandb.sweep(sweep_configuration, project="tft_sweep", entity="czyjtu")
+    if sweep_id is None:
+        input_chunk_length, output_chunk_length = INPUTS_DIMENSIONS[dataset]
+        train, val, covariates = get_series_and_covariates(dataset, False)
 
-    wandb.agent(sweep_id, lambda: entrypoint(dataset), count=15)
+        sweep_configuration = {
+            "name": f"tft_sweep_{dataset}",
+            "method": "bayes",
+            "metric": {"goal": "minimize", "name": "rmse_score"},
+            "parameters": {
+                "lr": {"values": [0.1, 0.01, 0.001]},
+                "batch_size": {"values": [64, 128, 256]},
+                "hidden_size": {"values": [16, 32, 64]},
+                "num_heads": {"values": [2, 4, 6]},
+                "full_attention": {"values": [True, False]},
+                "input_chunk_length": {
+                    "values": [
+                        int(output_chunk_length * i)
+                        for i in [2, 2.5, 3, 3.5, 4, 4.5, 5]
+                        if int(output_chunk_length * i) + output_chunk_length
+                        <= len(val)
+                    ]
+                },
+            },
+        }
+
+        sweep_id = wandb.sweep(
+            sweep_configuration, project="tft_sweep", entity="czyjtu"
+        )
+        wandb.agent(sweep_id, lambda: entrypoint(dataset), count=40)
+    else:
+        # get best parameters from sweep, train model and save it
+        api = wandb.Api()
+        sweep = api.sweep(sweep_id)
+
+        input_chunk_length, output_chunk_length = INPUTS_DIMENSIONS[dataset]
+        # Get best run parameters
+        best_run = sweep.best_run()
+        best_parameters = best_run.config
+        print(best_parameters)
+        print(best_run.id)
+        print(best_run.name)
+        hparams = HParams(
+            lr=best_parameters["lr"],
+            batch_size=best_parameters["batch_size"],
+            hidden_size=best_parameters["hidden_size"],
+            num_heads=best_parameters["num_heads"],
+            full_attention=best_parameters["full_attention"],
+            input_chunk_length=best_parameters["input_chunk_length"],
+            output_chunk_length=output_chunk_length,
+        )
+        train, val, covariates = get_series_and_covariates(dataset, True)
+        print(
+            input_chunk_length,
+            output_chunk_length,
+            len(train),
+            len(val),
+            len(covariates),
+        )
+        epochs = 20
+
+        model = get_model(
+            hparams,
+            epochs,
+            dataset.name,
+        )
+
+        model.fit(
+            train,
+            future_covariates=covariates,
+            epochs=epochs,
+            val_future_covariates=covariates,
+            # val_series=val,
+            verbose=True,
+            num_loader_workers=4,
+        )
+        model.save(str(MODEL_DIR / f"{model.model_name}"))
+        horizon = len(val)
+        pred_series = model.predict(
+            n=len(val),
+            num_samples=100,
+            trainer=Trainer(accelerator="cpu", precision="64"),
+            series=train,
+            future_covariates=covariates,
+        )
+
+        MAX_PLOT_HORIZON = 1000
+        MAX_PLOT_TRAIN = 100
+        rmse_score = rmse(val, pred_series)
+        plot_predictions(
+            train.values()[-MAX_PLOT_TRAIN:].flatten(),
+            val.values()[:MAX_PLOT_HORIZON].flatten(),
+            pred_series.mean().values()[:MAX_PLOT_HORIZON].flatten(),
+            dataset.name,
+            title=f"rmse={rmse_score}",
+        )
 
 
 if __name__ == "__main__":
